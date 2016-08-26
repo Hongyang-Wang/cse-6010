a48163d6cfd0286595fdb0638e0e3b6aa1b256bf
Hongyang-Wang <hongyangwang1992@gmail.com>
2016-08-26 17:13:28 -0400

    update

---
+++ 1-modeling-forest-fires/src/main.c
@@ -0,0 +1,47 @@
+/*
+ ============================================================================
+ Name        : main.c
+ Author      : Hongyang Wang
+ Version     : 1.0.0
+ Copyright   : © Fall 2016 CSE 6010 Hongyang Wang
+ Description : Cellular automation for forest fires
+ Date		 : Aug 25 - Sep 9, 2016
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(int argc, char *argv[]) {
+
+	// 1. get input args
+	if (argc != 6) {
+		printf("The program takes in 5 arguments: height, width, g, f and iter_num");
+		return -1;
+	}
+
+	int height = atoi(argv[1]), width = atoi(argv[2]), iter_num = atoi(argv[5]);
+	float g = atof(argv[3]), f = atof(argv[4]);
+	// log something
+
+	// 2. run the simulation
+	// a. initiate variables
+	int p[iter_num];  // store the P after each time step
+	bool grid[width][height];  // initiate the forest grid
+	// b. begin simulation
+	for (int i = 0; i < iter_num; i++) {
+		run_growth_phase(grid, g);
+		run_fire_phase(grid, f);
+		p[i] = compute_p(grid);
+	}
+
+	// 3. print the result
+	float avg_p = 0;
+	for (int i = 0; i < iter_num; i++) {
+		avg_p += p[i];
+	}
+	avg_p /= iter_num;
+
+	return 0;
+}
