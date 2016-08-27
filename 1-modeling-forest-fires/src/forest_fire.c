5a84d9fdd37c60c95aa5b21519e64c4218fe45af
Hongyang-Wang <hongyangwang1992@gmail.com>
2016-08-26 22:48:31 -0400

    update

--- 1-modeling-forest-fires/src/forest_fire.c
+++ 1-modeling-forest-fires/src/forest_fire.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-
 static double _gen_rand(void);
 static void _dfs(bool *grid, int height, int width, int i, int j);
+
+static const int NEIGHBORS[][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};  // north, south, west, east
 
 void run_growth_phase(bool *grid, int height, int width, double g) {
 	for (int i = 0; i < height; i++) {
@@ -56,8 +57,7 @@ static void _dfs(bool *grid, int height, int width, int i, int j) {
 	if (i >= height || i < 0 || j >= width || j < 0 || !*(grid + i * width + j))
 		return;
 	*(grid + i * width + j) = false;
-	static const int orientation[][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};  // north, south, west, east
 	for (int k = 0; k < 4; k++) {
-		_dfs(grid, height, width, i + orientation[k][0], j + orientation[k][1]);
+		_dfs(grid, height, width, i + NEIGHBORS[k][0], j + NEIGHBORS[k][1]);
 	}
 }
