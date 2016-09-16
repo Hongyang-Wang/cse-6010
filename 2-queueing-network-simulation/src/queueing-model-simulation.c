954c4f632f8f09bcb7e30eb515499bcbb091ce2d
Hongyang-Wang <hongyangwang1992@gmail.com>
2016-09-16 11:13:21 -0400

    create

---
+++ 2-queueing-network-simulation/src/queueing-model-simulation.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : queueing-model-simulation.c
+ Author      : Hongyang Wang
+ Version     :
+ Copyright   : © Fall 2016 CSE 6010 Hongyang Wang
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
