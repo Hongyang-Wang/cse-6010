54e3f836690c69bffa740342904048a7103fbb56
Hongyang-Wang <hongyangwang1992@gmail.com>
2016-09-23 11:38:46 -0400

    update

--- 2-queueing-network-simulation/src/event/event_handler.h
+++ 2-queueing-network-simulation/src/event/event_handler.h
@@ -9,8 +9,8 @@
 #define EVENT_EVENT_H_
 
 int event_cmp(void *d1, void *d2);
-void free_event(Event *event);
-void free_part(Part *part);
+void free_event(void *event);
+void free_part(void *part);
 void schedule_next_event(double start_time, double station_id, void (*event_handler)());
 void leave(Event *event);
 void start_service(Event *event);
