76e2e55e11b6d65ac7d7cc2dcfd7bbe9659765b6
NinjaSong <soniasong2525@hotmail.com>
2016-09-24 01:48:13 -0400

    Create PriorityQueue.h
    update with PQ.c

---
+++ 2-queueing-network-simulation/src/PriorityQueue.h
@@ -0,0 +1,28 @@
+/*
+ * PriorityQueue.h
+ *
+ *  Created on: Sep 22, 2016
+ *      Author: YS
+ */
+
+#ifndef UTILS_PRIORITY_QUEUE_PRIORITYQUEUE_H_
+#define UTILS_PRIORITY_QUEUE_PRIORITYQUEUE_H_
+
+typedef struct _pq_node {
+    Event *event;
+    struct _q_node *next;
+} PQNode;
+
+
+typedef struct _min_priority_queue {
+    	PQNode *head, *tail, *compare;
+} PriorityQueue;
+
+void pq_init(PriorityQueue *q);
+int pq_size(PriorityQueue *q);
+void pq_push(PriorityQueue *q, void *p);
+void *pq_pop(PriorityQueue *q);
+void *pq_peek(PriorityQueue *q);
+void pq_clear(PriorityQueue *q, void (*free_data)());
+
+#endif /* UTILS_PRIORITY_QUEUE_PRIORITYQUEUE_H_ */
